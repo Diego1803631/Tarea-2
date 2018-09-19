ff64fe02bc714cf5b1aeeae7adc207447046e83d
Diego Flores <42399052+Diego1803631@users.noreply.github.com>
2018-09-18 21:55:24 -0500

    Tarea 11-Estructuras
    Tengo un problema con la salida del programa.

--- Proyecto final/3631.cpp
+++ Proyecto final/3631.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<conio.h>
+#include <string>
 #include<windows.h>
+#include<fstream>
 using namespace std;
 int gotoxy(int x, int y)
 {
@@ -19,8 +21,27 @@ void ediciondealumnos();
 void borraralumno();
 void manualdeusuario();
 void listadealumnos();
-int op;
 char salida;
+/////////////////////////////
+struct datos {
+	char nombres[100];
+	char apellidos[100];
+	char correo[100];
+	char telefono[100];
+	char matricula[100];
+	char calle[100];
+	char numerodecasa[100];
+	char colonia[100];
+	int cal1;
+	int cal2;
+	int cal3;
+};
+datos alumno[100];
+/////////////////////////////
+int i = 0;
+int j = 0;
+int op;
+int numerodealumnos;
 int main() {
 	locale::global(locale("spanish"));
 	menu:
@@ -39,10 +60,11 @@ int main() {
 	cin >> op;
 	switch (op) {
 	case 1:
+		system("cls");
 		altadealumnos();
 		cout << "Regresar al menú ¿s/n?" << endl;
 		cin >> salida;
-		if ((salida =='s')||(salida == 'S')) {
+		if ((salida == 's') || (salida == 'S')) {
 			goto menu;
 		}
 		else {
@@ -104,6 +126,7 @@ int main() {
 			cout << "De momento no se puede hacer otra cosa así que BREAK :c" << endl;
 		}
 		break;
+		//no detecto el error, se rompe el programa cuando ingreso un nombre y regreso al menu ya no me deja salir. 
 	default:
 		cout << "Desea salir ¿s/n?" << endl;
 		cin >> salida;
@@ -113,20 +136,120 @@ int main() {
 		else {
 			goto menu;
 		}
-		break;
 	}
 	_getch();
 	return 0;
 }
 void altadealumnos() {
-	system("cls");
-	gotoxy(7, 0);
-	cout << "----------Alta de alumnos----------" << endl;
+	menu2:
+		gotoxy(7, 0);
+		cout << "----------Alta de alumnos----------" << endl;
+		cout << "1.Nombre/s:" << endl;
+		cout << "2.Apellido/s:" << endl;
+		cout << "3.Correo electrónico:" << endl;
+		cout << "4.Teléfono:" << endl;
+		cout << "5.Matrícula:" << endl;
+		cout << "6.Dirección[Calle]:" << endl;
+		cout << "7.Dirección[Número]:" << endl;
+		cout << "8.Dirección[Colonia]:" << endl;
+		cout << "9.Ingresar otro alumno." << endl;
+		cout << "10.Salir." << endl;
+		cout << "Escoga una opción [  ]" << endl;
+			gotoxy(19, 11);
+			cin >> op;
+			switch (op) {
+			case 1:
+				while (1) {
+					gotoxy(11, 1);
+					cin.ignore();
+					cin.getline(alumno[i].nombres, 100);
+					_strupr_s(alumno[i].nombres);
+					cout << alumno[i].nombres << endl;
+					numerodealumnos++;
+					altadealumnos();
+				}
+				break;
+			case 2:
+				while (1) {
+					gotoxy(13, 2);
+					cin.ignore();
+					cin.getline(alumno[i].apellidos, 100);
+					_strupr_s(alumno[i].apellidos);
+					cout << alumno[i].apellidos << endl;
+					altadealumnos();
+				}
+				break;
+			case 3:
+				while (1) {
+					gotoxy(21, 3);
+					cin.ignore();
+					cin.getline(alumno[i].correo, 100);
+					cout << alumno[i].correo << endl;
+					altadealumnos();
+				}
+				break;
+			case 4:
+				while (1) {
+					gotoxy(11, 4);
+					cin.ignore();
+					cin.getline(alumno[i].telefono, 100);
+					cout << alumno[i].telefono << endl;
+					altadealumnos();
+				}
+				break;
+			case 5:
+				while (1) {
+					gotoxy(12, 5);
+					cin.ignore();
+					cin.getline(alumno[i].matricula, 100);
+					cout << alumno[i].matricula << endl;
+					altadealumnos();
+				}
+				break;
+			case 6:
+				while (1) {
+					gotoxy(19, 6);
+					cin.ignore();
+					cin.getline(alumno[i].calle, 100);
+					cout << alumno[i].calle << endl;
+					altadealumnos();
+				}
+				break;
+			case 7:
+				while (1) {
+					gotoxy(20, 7);
+					cin.ignore();
+					cin.getline(alumno[i].numerodecasa, 100);
+					cout << alumno[i].numerodecasa << endl;
+					altadealumnos();
+				}
+				break;
+			case 8:
+				while (1) {
+					gotoxy(21, 8);
+					cin.ignore();
+					cin.getline(alumno[i].colonia, 100);
+					cout << alumno[i].colonia << endl;
+					altadealumnos();
+				}
+				break;
+			case 9:
+				system("cls");
+				i++;
+				altadealumnos();
+				break;
+
+			default:
+				break;
+			}
+			main();
+			
 }
 void altadecalificaciones() {
 	system("cls");
 	gotoxy(7, 0);
 	cout << "----------Alta de calificaciones----------" << endl;
+
 }
 void ediciondealumnos() {
 	system("cls");
@@ -146,4 +269,8 @@ void manualdeusuario() {
 void listadealumnos() {
 	system("cls");
 	cout << "----------Lista de alumnos y calificaciones----------" << endl;
+	for (i = 0; i < numerodealumnos; i++) {
+		cout << "\n[" << i << "]=" << alumno[i].nombres << " " << alumno[i].apellidos << " " << alumno[i].matricula << endl;
+	}
+	
 }
